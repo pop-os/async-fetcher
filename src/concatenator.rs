0d265978fca5c4291ae752c293c9ab7e63c3c2b0
Michael Aaron Murphy <mmstick@pm.me>
2022-02-10 20:11:38 +0100

    fix: When concatenating, perform flush regardless of error in write

--- src/concatenator.rs
+++ src/concatenator.rs
@@ -13,14 +13,19 @@ pub async fn concatenator<P>(dest: &mut File, mut parts: P) -> Result<(), Error>
 where
     P: Stream<Item = Result<Arc<Path>, Error>> + Send + Unpin,
 {
-    while let Some(task_result) = parts.next().await {
-        let part_path: Arc<Path> = task_result?;
-        concatenate(dest, part_path).await?;
+    let result = async {
+        while let Some(task_result) = parts.next().await {
+            let part_path: Arc<Path> = task_result?;
+            concatenate(dest, part_path).await?;
+        }
+
+        Ok(())
     }
+    .await;
 
     let _ = dest.flush().await;
 
-    Ok(())
+    result
 }
 
 /// Concatenates a part into a file.
